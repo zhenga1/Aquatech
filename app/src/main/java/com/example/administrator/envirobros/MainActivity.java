c99bfc3d7816c8a2d984faed528cd727771e92a3
Aaron Zheng <aaronhaowenzheng@gmail.com>
2020-11-22 01:36:05 +0800

    Second working. customised the listviews and added markers on the maps and adjusted the map zoom.

--- app/src/main/java/com/example/administrator/envirobros/MainActivity.java
+++ app/src/main/java/com/example/administrator/envirobros/MainActivity.java
@@ -1,31 +1,38 @@
 package com.example.administrator.envirobros;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final int NUM_ROWS = 3;
-    private TextView title;
-    private String[] string = new String[]{"Temperature", "Ph", "Conductivity"};
+    private String[] title = new String[]{"Temperature", "Ph", "Conductivity"};
+    private String[] descriptions = new String[]{"Here is the values collected for temperature", "Here is the values collected for pH", "Here is the values collected for conductivity"};
+    private int[] images = {R.drawable.heat,R.drawable.ph, R.drawable.conductivity};
     private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //title = findViewById(R.id.textView);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, string);
+        MyAdapter adapter = new MyAdapter(this, title,descriptions,images);
         listView = findViewById(R.id.listView);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -37,6 +44,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    class MyAdapter extends ArrayAdapter<String>{
+        Context context;
+        String rtitle[];
+        String rdescription[];
+        int rImgs[];
+        MyAdapter(Context c, String title[], String descrip[], int img[])
+        {
+            super(c, R.layout.row, R.id.heading, title);
+            this.context=c;
+            this.rtitle=title;
+            this.rdescription=descrip;
+            this.rImgs=img;
+
+        }
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row,parent,false);
+            ImageView imageView = row.findViewById(R.id.image);
+            TextView titleTextview = row.findViewById(R.id.heading);
+            TextView subtitletextview = row.findViewById(R.id.subtitle);
+
+            imageView.setImageResource(rImgs[position]);
+            titleTextview.setText(rtitle[position]);
+            subtitletextview.setText(rdescription[position]);
+
+            return row;
+
+        }
+    }
+
 }
 
 /*
--- app/src/main/java/com/example/administrator/envirobros/MapsActivity.java
+++ app/src/main/java/com/example/administrator/envirobros/MapsActivity.java
@@ -1,21 +1,34 @@
 package com.example.administrator.envirobros;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private LatLng userloc = new LatLng(22.28, 114.16);
+    private LatLng mls = new LatLng(22.43060133,114.20836061);
+    private LatLng wks = new LatLng(22.43004101,114.23630923);
+    private LatLng tungchung = new LatLng(22.29424854,113.94256443);
+    FusedLocationProviderClient fusedLocationProviderClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +53,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setMinZoomPreference(15);
+        mMap.setMaxZoomPreference(20);
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if(ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+                    if(location!=null){
+                        userloc = new LatLng(location.getLatitude(),location.getLongitude());
+                    }
+                }
+            });
+        }else{
+            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+
+        }
+        mMap.addMarker(new MarkerOptions().position(tungchung));
+        mMap.addMarker(new MarkerOptions().position(mls));
+        mMap.addMarker(new MarkerOptions().position(wks));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(userloc));
     }
 }
